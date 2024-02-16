0c9264e4c9e424ec1e5973c722f4286a9b2cd340
Tarun <92459398+pokemondodge@users.noreply.github.com>
2024-02-16 16:12:41 +0530

    Update and rename emp.java to empServlet.java

--- empServlet.java
+++ empServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class emp
  */
 @WebServlet("/emp")
-public class emp extends HttpServlet {
+public class empServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
